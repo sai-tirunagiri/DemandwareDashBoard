9647a9e370e90b41b6160cc6d5db623e98d436e9
sai-tirunagiri <sai.tirunagiri@gmail.com>
2016-04-13 22:17:44 +0530

    Revert "April"
    This reverts commit e7e64a362e7d2330543d61608fcb39714a55a388.

--- FileDashBoard/WebContent/scripts/tablePaginate.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function(){
-                		$('#records_table').tablePaginate({navigateType:'full',recordPerPage:2});
-      });
---
+++ src/org/quinnox/controller/ValidateLoginCredentials.java
@@ -0,0 +1,51 @@
+package org.quinnox.controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.quinnox.dao.DashBoardDAO;
+
+/**
+ * Servlet implementation class ValidateLoginCredentials
+ */
+public class ValidateLoginCredentials extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String userName = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		if(userName.equalsIgnoreCase("dashboard")&&password.equalsIgnoreCase("dashboard")){
+			response.sendRedirect("home.jsp");
+		}
+		else
+		{
+			response.sendRedirect("login.html");
+		}
+		
+		/*DashBoardDAO dashBoardDAO = new DashBoardDAO();
+		try {
+			if(dashBoardDAO.checkDBConnectionCredentials(userName, password)){
+				response.sendRedirect("home.jsp");
+			}
+			else{
+				response.sendRedirect("login.html");
+			}
+		} catch (ClassNotFoundException e) {
+			response.sendRedirect("ErrorPage.jsp");
+			e.printStackTrace();
+		} catch (SQLException e) {
+			response.sendRedirect("ErrorPage.jsp");
+			e.printStackTrace();
+		}*/
+	}
+
+}
